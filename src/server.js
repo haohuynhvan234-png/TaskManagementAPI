a6d8a8dd3d7435ae7f841dfcf7831d049be30276
haohuynhvan234-png <haohuynhvan234@gmail.com>
2026-09-13 17:00:39 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -82,3 +82,35 @@ if (process.env.MONGO_URI) {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`🚀 Server: http://localhost:${PORT}`));
+
+import cors from "cors";
+const allowedOrigins = [
+  "http://localhost:3000",
+
+  // Vercel frontend
+  "https://task-management-kislai.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Cho phép request không có Origin
+      // Ví dụ: Postman, Swagger, server-to-server
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+
+    allowedHeaders: ["Content-Type", "Authorization"],
+
+    credentials: true,
+  }),
+);
